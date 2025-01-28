75222aba0daedb5b9e26ccc6cace5c3d2c1c9eb0
thompa <thompa@umich.edu>
2025-01-28 17:06:17 -0500

    Implement session logic

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, session, render_template, redirect, url_for
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from dotenv import load_dotenv
 import os
@@ -21,35 +21,47 @@ def generate_code():
             break
     return code
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    session.clear()
+    if request.method == 'POST':
+        username = request.form.get('username')
+        action = request.form.get('action')
+
+        if not username or username.strip() == "":
+            context = { "err": True, "msg": "Enter a valid username" }
+            return render_template('index.html', ctx=context)
+        if action == "Create Room":
+            room = generate_code()
+            rooms[room] = {"total_players": 0, "players": []}
+            session["room"] = room
+            session["name"] = username
+            return redirect(url_for('lobby', code=room))
+        elif action == "Join Room":
+            code = request.form.get('join-code')
+            if code in rooms:
+                session["room"] = code
+                session["name"] = username
+                return redirect(url_for('lobby', code=code))
+            else: 
+                context = { "err": True, "msg": "Room does not exist" }
+                return render_template('index.html', ctx=context)
     context = { "err": False, "msg": "" }
     return render_template('index.html', ctx=context)
 
-@app.route('/room_action', methods=['POST'])
-def room_action():
-    username = request.form.get('username')
-    action = request.form.get('action')
-
-    if not username or username.strip() == "":
-        context = { "err": True, "msg": "Enter a valid username" }
-        return render_template('index.html', ctx=context)
-
-    if action == "Create Room":
-        return redirect(url_for('lobby', code=generate_code()))
-    
-    elif action == "Join Room":
-        code = request.form.get('join-code')
-        if code in rooms:
-            return redirect(url_for('lobby', code=code))
-        else: 
-            context = { "err": True, "msg": "Room does not exist" }
-            return render_template('index.html', ctx=context)
-
-
-@app.route('/lobby/<code>')
+@app.route('/lobby/<code>', methods=['GET', 'POST'])
 def lobby(code):
-    return render_template('lobby.html', code=code)
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("index"))
+    
+    if request.method == "POST":
+        return redirect(url_for('index'))
+    
+    context={
+                "code": code
+            }
+    return render_template('lobby.html', ctx=context)
 
 @socketio.on('connect')
 def user_connect(): 
