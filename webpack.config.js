13bcc95d21553089859df70c0019fd006489669d
thompa <thompa@umich.edu>
2025-02-08 16:30:34 -0500

    Configure basic bar graph

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {  
+    mode: 'development',  
+    entry: './static/js/index.js',  
+    output: {  
+      filename: 'main.js',  
+      publicPath: 'dist'  
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js'],
+    },
+  };
